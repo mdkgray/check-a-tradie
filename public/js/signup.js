65727fb38efe36217fd6395fac5cbb3d6f4ba50c
Tanel Colak <88058585+kreatifbob@users.noreply.github.com>
2022-08-21 16:44:06 +1000

    Update signup.js
    revert changes to make easier to read

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,9 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#exampleFormControlInput1').value.trim();
-    const email = document.querySelector('#formGroupExampleInput').value.trim();
-    const password = document.querySelector('#exampleInputPassword1').value.trim();
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
   
     if (name && email && password) {
       const response = await fetch('/api/users', {
